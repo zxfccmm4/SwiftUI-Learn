3651d2e6f97ce84d5fcbb193eb6e6754380cdf3a
Steve Zhou <zxfccmm@126.com>
2022-09-22 02:04:07 +0800

    Add files via upload

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,93 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Steve on 2022/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var emojis = ["✈️","🚔","🚘","🚝","⛴️","💺","🚗","🚕","🚙","🚌","🚎","🏎️","🚓","🚑","🚐","🛻","🚚","🚛","🚜","🛴","🚲","🛵","🏍️","🛺","🛞","🚨","🚋","🚇","🚅"]
+    
+    @State  var emojiCount = 4
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))]){
+                        ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                            CardView(content: emoji)
+                                .aspectRatio(2/3, contentMode: .fit)
+                        }
+                }
+            }
+
+            .foregroundColor(.red)
+                Spacer()
+                HStack{
+                    remove
+                    Spacer()
+                    add
+                    
+                }
+                    .font(.largeTitle)
+                    .padding(.horizontal)
+                
+            }
+            .padding(.horizontal)
+
+        }
+        var remove: some View {
+            Button {
+                if emojiCount > 1 {
+                    emojiCount -= 1
+                }
+            } label: {
+                Image(systemName: "minus.circle")
+            }
+        }
+        
+        var add: some View {
+            Button {
+                if emojiCount < emojis.count {
+                    emojiCount += 1
+                }
+            } label: {
+                Image(systemName: "plus.circle")
+            }
+        }
+    }
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
+    var body: some View{
+        ZStack{
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape
+                    .fill()
+                    .foregroundColor(.white)
+                shape
+                    .strokeBorder(lineWidth: 3)
+                Text(content)
+                    .font(.largeTitle)
+            } else {
+                shape
+                    .fill()
+            }
+            
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+        
+    }
+    
+}
---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,16 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Steve on 2022/9/20.
+//
+
+import SwiftUI
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
